22731beb7186d2bdb24fbac7b6561be2c5f963bb
Rohan <rohandop@gmail.com>
2012-07-25 13:18:40 +0200

    init

---
+++ GW2G_IOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GW2G_IOS
+//
+//  Created by Rohan Doppenberg on 7/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GW2G_IOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GW2G_IOS
+//
+//  Created by Rohan Doppenberg on 7/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
